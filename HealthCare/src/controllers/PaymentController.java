53ea187516766c7b7bf3d300d3bb07bacad89b80
Rashini <rashini67@gmail.com>
2020-04-11 16:39:31 +0530

    PaymentController

--- HealthCare/src/controllers/PaymentController.java
+++ HealthCare/src/controllers/PaymentController.java
@@ -1,4 +1,34 @@
 package controllers;
 
+import java.sql.SQLException;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+
+import beans.APaymentBean;
+import beans.CommonResponseBean;
+import services.PaymentService;
+import util.DBConnection;
+
+@Path("payment")
 public class PaymentController {
+	private PaymentService paymentService;
+
+	public PaymentController() throws ClassNotFoundException, SQLException {
+		this.paymentService = new PaymentService(DBConnection.connect());
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("make")
+	public CommonResponseBean makePayment(APaymentBean payment) {
+		try {
+			this.paymentService.makePayment(payment);
+			return CommonResponseBean.OK("Success");
+		} catch (Exception e) {
+			return CommonResponseBean.Error(e);
+		}
+	}
 }

bdd81ea4efebdaa64f24c4578290a5b1020de5dd
vidushivimansa <vidushisedara@gmail.com>
2020-04-17 03:26:33 -0700

    Security filter added

--- HospitalService/src/com/HospitalService.java
+++ HospitalService/src/com/HospitalService.java
@@ -3,6 +3,7 @@ package com;
 import java.util.List;
 
 import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -12,7 +13,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
@@ -20,63 +20,55 @@ import beans.HospitalBean;
 import model.Hospital;
 
 @Path("/hospitals")
-@PermitAll
-public class HospitalService 
-{
+
+public class HospitalService {
 	Hospital hospitalObj = new Hospital();
-	
+
+	@RolesAllowed("admin")
 	@GET
 	@Path("/")
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<HospitalBean> readHospital() 
-	{
+	public List<HospitalBean> readHospital() {
 		return hospitalObj.readHospital();
-		
+
 	}
-	
 
 	@POST
 	@Path("/")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.TEXT_PLAIN)
-	public String insertHospital(String hospitalData) 
-	{
+	public String insertHospital(String hospitalData) {
 		HospitalBean hos = new HospitalBean(hospitalData);
-		
-		String output =	hospitalObj.insertHospital(hos);
+
+		String output = hospitalObj.insertHospital(hos);
 		return output;
-		
-	} 
-	
-	
+
+	}
+
 	@PUT
 	@Path("/")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.TEXT_PLAIN)
-	public String updateHospital(String hospitalData) 
-	{
+	public String updateHospital(String hospitalData) {
 		HospitalBean hos = new HospitalBean(hospitalData);
-		
-		String output =	hospitalObj.updateHospital(hos);
+
+		String output = hospitalObj.updateHospital(hos);
 		return output;
-		
+
 	}
-	
-	
+
 	@DELETE
 	@Path("/")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.TEXT_PLAIN)
-	public String deleteHospital(String hospitalData) 
-	{
+	public String deleteHospital(String hospitalData) {
 		JsonObject hospitalObject = new JsonParser().parse(hospitalData).getAsJsonObject();
-		
+
 		String h_ID = hospitalObject.get("h_ID").getAsString();
-		
+
 		String output = hospitalObj.deleteHospital(h_ID);
 		return output;
-		
+
 	}
-	
 
 }

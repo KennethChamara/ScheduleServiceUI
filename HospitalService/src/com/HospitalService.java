b19f197300d42ffbd4f8d37ad02510e9c76241b3
vidushivimansa <vidushisedara@gmail.com>
2020-04-17 06:03:23 -0700

    HospitalServiceUpdated

--- HospitalService/src/com/HospitalService.java
+++ HospitalService/src/com/HospitalService.java
@@ -10,6 +10,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -35,6 +36,17 @@ public class HospitalService {
 
 	}
 	
+	
+	// read a hospital identified by id
+	@RolesAllowed({ "admin", "doctor","patient" })
+	@GET
+	@Path("/{h_ID}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public HospitalBean readHospitalById(@PathParam("h_ID") int id) {
+		return hospitalObj.readHospitalById(id);
+	}
+	
+	
 	//insert hospital
 	@RolesAllowed("admin")
 	@POST
@@ -48,6 +60,7 @@ public class HospitalService {
 		return output;
 
 	}
+	
 	
 	//update hospital
 	@RolesAllowed("admin")
